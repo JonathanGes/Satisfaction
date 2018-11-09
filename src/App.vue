377615c43e3b71e20f9954a474a9cfa4cd1733be
Jonathan Gescheit <jonathan.ges@gmail.com>
2018-11-09 21:19:48 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <SatisfiedButton></SatisfiedButton>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import SatisfiedButton from "./components/SatisfiedButton.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    SatisfiedButton
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SatisfiedButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="satisfied-button">
+    <span>Satisfaction</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SatisfiedButton"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+span {
+  color: red;
+}
+</style>
